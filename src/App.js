f460a482ee42c0a7461ecaeb3bcf01cc16128ff5
David D <dailleredavid@gmail.com>
2023-11-09 21:16:17 +0530

    revert to browserrouter

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import { Route, Routes, HashRouter } from 'react-router-dom';
-// Style
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+//Style
 import './App.css';
-// Layout
+//Layout
 import AppLayout from './AppLayout';
-// Pages
+//Pages
 import Home from './page/home/Home';
 import About from './page/about/About';
 import Projects from './page/projects/Projects';
 import Resume from './page/resume/Resume';
-// Components
-import Menu from './components/menu/menu';
+//Components
+import Menu from './components/menu/menu'
 import ScrollToTop from './components/scrollTop/ScrollTop';
-
+ 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <ScrollToTop />
       <Routes>
         <Route path='/' element={<AppLayout />}>
           {/* Route for other languages */}
-          <Route path='#/:lang/*' element={<Home />} />
-          <Route path='#/:lang/about' element={<About />} />
-          <Route path='#/:lang/projects' element={<Projects />} />
-          <Route path='#/:lang/resume' element={<Resume />} />
-          <Route path='#/:lang/menu' element={<Menu />} />
-
-          <Route path='/' element={<Home />} />
+          <Route path=':lang/*' element={<Home />} />
+          <Route path=':lang/about' element={<About />} />
+          <Route path=':lang/projects' element={<Projects />} />
+          <Route path=':lang/resume' element={<Resume />} />
+          <Route path=':lang/menu' element={<Menu />} />
+  
+           <Route path='/' element={<Home />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
